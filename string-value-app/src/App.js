f197b4f983ca4d797203ffce9aa92ee9ebc694cd
fitzgallo <fitzgerald_gallo@rcc.edu.ph>
2024-09-28 10:09:38 +0800

    StringValue

---
+++ string-value-app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+
+function App() {
+  const message = ["Im Fitzgerald B. Gallo"];
+  
+
+  return (
+    <div className="App">
+      <h1>{message}</h1>
+    </div>
+  );
+}
+
+export default App;
